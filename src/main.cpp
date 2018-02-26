0d99c6356afe4eca6946c00b683a5fb9806de7a2
vincentlaucsb <vincentla@umail.ucsb.edu>
2018-02-26 09:49:28 -0800

    SVG drawings now have correct width/height/viewBox set

--- src/main.cpp
+++ src/main.cpp
@@ -19,36 +19,6 @@ void draw_tree(SVG::Group& edges, SVG::Group& vertices, TreeNode& tree) {
 }
 
 int main(int argc, char** argv) {
-    /*
-    // Create a binary tree of depth 2
-    TreeNode t1, t2;
-    binary_tree(&t1, 2);
-    binary_tree(&t2, 5);
-    t1.calculate_xy();
-    t2.calculate_xy();
-    
-    std::cout << t1.displacement << std::endl;
-    std::cout << t1.left->displacement << " - " << t1.right->displacement << std::endl;
-    std::cout << t1.left->left->displacement << ", " << t1.left->right->displacement <<
-        " - " << t1.right->left->displacement << ", " << t1.right->right->displacement << std::endl << std::endl;
-
-    std::cout << t2.displacement << std::endl;
-    std::cout << t2.left->displacement << " - " << t2.right->displacement << std::endl;
-    std::cout << t2.left->left->displacement << ", " << t2.left->right->displacement <<
-        " - " << t2.right->left->displacement << ", " << t2.right->right->displacement << std::endl;
-
-    // SVG
-    SVG::SVG root;
-    SVG::Group edges, nodes;
-    edges.set_attr("stroke", "black")
-        .set_attr("stroke-width", "1px");
-
-    std::ofstream outfile("tree.svg");
-    draw_tree(edges, nodes, t2);
-    root.add_child(edges, nodes);
-    outfile << root.to_string();
-    */
-    
     if (argc < 3) {
         std::cout << "Usage: " << argv[0] << " [output file] [depth]" << std::endl
             << "Produces a binary tree of specified depth" << std::endl;
@@ -62,11 +32,12 @@ int main(int argc, char** argv) {
     SVG::SVG root;
     SVG::Group edges, nodes;
     edges.set_attr("stroke", "black")
-        .set_attr("stroke-width", "1px");
+         .set_attr("stroke-width", "1px");
 
     std::ofstream outfile(argv[1]);
     draw_tree(edges, nodes, tree);
     root.add_child(edges, nodes);
+    root.set_bbox();
     outfile << root.to_string();
     
     return 0;
